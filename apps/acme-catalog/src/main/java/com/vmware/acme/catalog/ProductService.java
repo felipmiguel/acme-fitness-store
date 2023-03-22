0a4cd37bf2e4282c2647345756b5e79e88120f0f
felipmiguel <fmiguel@microsoft.com>
2023-03-22 10:08:11 +0100

    caffeine cache

--- apps/acme-catalog/src/main/java/com/vmware/acme/catalog/ProductService.java
+++ apps/acme-catalog/src/main/java/com/vmware/acme/catalog/ProductService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.cache.annotation.Cacheable;
 
 import static java.util.stream.StreamSupport.stream;
 
@@ -18,11 +19,13 @@ public class ProductService {
 		this.productRepository = productRepository;
 	}
 
+	@Cacheable(cacheNames= "products")
 	public List<Product> getProducts() {
 		return stream(productRepository.findAll().spliterator(), false)
 				.collect(Collectors.toList());
 	}
 
+	@Cacheable(cacheNames= "product-items", key="#id")
 	public Product getProduct(String id) {
 		return productRepository.findById(id)
 								.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find product with id " + id));
